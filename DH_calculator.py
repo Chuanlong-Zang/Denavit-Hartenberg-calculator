b9186ce1de6f988b862002c6b72e3de73c6da262
Chuanlong-Dragon <zang.chuanlong@outlook.com>
2022-04-25 14:05:07 +0200

    Update DH_calculator.py

--- DH_calculator.py
+++ DH_calculator.py
@@ -2,47 +2,21 @@
 """
 Created on Thu Jan 21 16:00:54 2021
 
-@author: 14649
+@author: Chuanlong Zang
 """
-"""
-import sympy
-from sympy import symbols
-sympy.init_printing()
-x_1, x_2, x_3, x_4, x_5, x_6 = symbols('x_1 x_2 x_3 x_4 x_5 x_6')  # 定义自变量x_1, x_2, x_3, x_4, x_5, x_6
-alpha, beta, x, y, gamma = symbols('alpha beta x y gamma')  # 定义参数alpha, beta, x, y, gamma
-system = sympy.Matrix((  # 方程参数矩阵，为系数阵的增广矩阵
-    (1, 0, x-1, -x, 0, 0, 0),
-    (0, 1, 0, 0, -y, y-1, 0),
-    (-gamma, 0, 1, 0, 0, 0, 2),
-    ((alpha-1)*gamma, -alpha*gamma, 0, 1, 0, 0, -4*alpha+3),
-    (-beta*gamma, (beta-1)*gamma, 0, 0, 1, 0, 4*beta-2),
-    (0, -gamma, 0, 0, 0, 1, -1)
-))
-a = sympy.solve_linear_system(system,  # 方程求解函数
-                          x_1, x_2, x_3, x_4, x_5, x_6)
 
-for key, value in a.items():  # 输出方程结果
-    print('{key}={value}'.format(key=key, value=value))
-
-
-"""
 import sympy
 #import numpy as np
 sympy.init_printing()
 from sympy import symbols,pprint
 
-"""
-np.pi / 2
-sympy.pi
-sympy.cos()
-sympy.sin()
-"""
-
+############# region 1: Here is nothing to be changed if you are computing a 4-link robot arm #############
 alpha_0, a_0, d_1, theia_1 = symbols('alpha_0 a_0 d_1 theia_1') # i = 1
 alpha_1, a_1, d_2, theia_2 = symbols('alpha_1 a_1 d_2 theia_2') # i = 2
 alpha_2, a_2, d_3, theia_3 = symbols('alpha_2 a_2 d_3 theia_3') # i = 3
 alpha_3, a_3, d_4, theia_4 = symbols('alpha_3 a_3 d_4 theia_4') # i = 4
 
+# classic DH transformation
 T_0_1 = sympy.Matrix([[sympy.cos(theia_1), -sympy.sin(theia_1), 0, a_0], 
                       [sympy.sin(theia_1)*sympy.cos(alpha_0), sympy.cos(theia_1)*sympy.cos(alpha_0), -sympy.sin(alpha_0), -sympy.sin(alpha_0)*d_1],
                       [sympy.sin(theia_1)*sympy.sin(alpha_0), sympy.cos(theia_1)*sympy.sin(alpha_0), sympy.cos(alpha_0), sympy.cos(alpha_0)*d_1],
@@ -62,8 +36,45 @@ T_3_4 = sympy.Matrix([[sympy.cos(theia_4), -sympy.sin(theia_4), 0, a_3],
                       [sympy.sin(theia_4)*sympy.cos(alpha_3), sympy.cos(theia_4)*sympy.cos(alpha_3), -sympy.sin(alpha_3), -sympy.sin(alpha_3)*d_4],
                       [sympy.sin(theia_4)*sympy.sin(alpha_3), sympy.cos(theia_4)*sympy.sin(alpha_3), sympy.cos(alpha_3), sympy.cos(alpha_3)*d_4],
                       [0, 0, 0, 1]])
+############# region 1 ended #############
 
+
+############# region 2: Here you need to input your known parameters (real numbers), if any #############
+
+""" something that might be frequently used
+np.pi / 2
+sympy.pi
+sympy.cos()
+sympy.sin()
+sympy.sqrt(2)
 """
+
+# if you want something parameterized, comment the parameters as below!
+T_0_1_eva=T_0_1.evalf(subs={alpha_0:0,
+                        a_0:2,
+                        d_1:0,
+                        #theia_1:-sympy.pi/2
+                        })
+
+T_1_2_eva=T_1_2.evalf(subs={alpha_1:-sympy.pi/2,
+                        a_1:2,
+                        d_2:0,
+                        #theia_2:0
+                        })
+
+T_2_3_eva=T_2_3.evalf(subs={alpha_2:-sympy.pi/2,
+                        a_2:2,
+                        d_3:0,
+                        #theia_3:sympy.pi
+                        })
+
+T_3_4_eva=T_3_4.evalf(subs={alpha_3:sympy.pi/2,
+                        a_3:2,
+                        d_4:0,
+                        #theia_4:0
+                        })
+
+""" initial state (all 0 - no rotation and no translation)
 
 T_0_1_eva=T_0_1.evalf(subs={alpha_0:0,
                         a_0:0,
@@ -89,32 +100,10 @@ T_3_4_eva=T_3_4.evalf(subs={alpha_3:0,
                         theia_4:0
                         })
 """
+############# region 2 ended #############
 
-# sympy.pi sympy.sqrt(2)
 
-T_0_1_eva=T_0_1.evalf(subs={alpha_0:0,
-                        a_0:2,
-                        d_1:0,
-                        #theia_1:-sympy.pi/2
-                        })
-
-T_1_2_eva=T_1_2.evalf(subs={alpha_1:-sympy.pi/2,
-                        a_1:2,
-                        d_2:0,
-                        #theia_2:0
-                        })
-
-T_2_3_eva=T_2_3.evalf(subs={alpha_2:-sympy.pi/2,
-                        a_2:2,
-                        d_3:0,
-                        #theia_3:sympy.pi
-                        })
-
-T_3_4_eva=T_3_4.evalf(subs={alpha_3:sympy.pi/2,
-                        a_3:2,
-                        d_4:0,
-                        #theia_4:0
-                        })
+############# region 3: Here you need to adjust your output if you don't need all outputs! #############
 
 T_0_2=T_0_1_eva*T_1_2_eva
 
@@ -134,8 +123,6 @@ pprint(sympy.N(T_2_3_eva,2))
 print("T_3_4 is :")
 pprint(sympy.N(T_3_4_eva,2))
 
-
-"""
 print("T_0_2 is :")
 pprint(sympy.N(T_0_2,2))
 
@@ -144,12 +131,6 @@ pprint(sympy.N(T_0_3,2))
 
 print("T_0_4 is :")
 pprint(sympy.N(T_0_4,2))
-"""
 
-#M= sympy.Matrix([[d_1, -1], [3, 4], [0, 2]])
-#print(np.sin(np.pi / 2))
-#print(expr.evalf(subs={x:sympy.pi}))
-#N = sympy.Matrix([1, 3])
-#print(M*N)
-#x = symbols('x')
-#expr = sympy.sin(x)
+############# region 3 ended #############
+
